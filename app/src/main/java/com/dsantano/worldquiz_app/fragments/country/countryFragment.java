4caa4c9b5663bf0a11317ae574de1ea1af982816
Jesús Ceacero Jimeno <jceacero@AULAS.TRIANA>
2020-02-03 14:28:33 +0100

    Inicio de fragment para listado de paises.

--- app/src/main/java/com/dsantano/worldquiz_app/fragments/country/countryFragment.java
+++ app/src/main/java/com/dsantano/worldquiz_app/fragments/country/countryFragment.java
@@ -12,35 +12,24 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.dsantano.worldquiz_app.Interfaces.ICountryListener;
 import com.dsantano.worldquiz_app.R;
 import com.dsantano.worldquiz_app.fragments.country.dummy.DummyContent;
 import com.dsantano.worldquiz_app.fragments.country.dummy.DummyContent.DummyItem;
 
 import java.util.List;
 
-/**
- * A fragment representing a list of Items.
- * <p/>
- * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
- * interface.
- */
 public class countryFragment extends Fragment {
 
-    // TODO: Customize parameter argument names
     private static final String ARG_COLUMN_COUNT = "column-count";
-    // TODO: Customize parameters
     private int mColumnCount = 1;
-    private OnListFragmentInteractionListener mListener;
+    private ICountryListener mListener;
+    private MycountryRecyclerViewAdapter adapter;
 
-    /**
-     * Mandatory empty constructor for the fragment manager to instantiate the
-     * fragment (e.g. upon screen orientation changes).
-     */
+
     public countryFragment() {
     }
 
-    // TODO: Customize parameter initialization
-    @SuppressWarnings("unused")
     public static countryFragment newInstance(int columnCount) {
         countryFragment fragment = new countryFragment();
         Bundle args = new Bundle();
@@ -81,11 +70,11 @@ public class countryFragment extends Fragment {
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnListFragmentInteractionListener) {
-            mListener = (OnListFragmentInteractionListener) context;
+        if (context instanceof ICountryListener) {
+            mListener = (ICountryListener) context;
         } else {
             throw new RuntimeException(context.toString()
-                    + " must implement OnListFragmentInteractionListener");
+                    + " must implement ICountryListener");
         }
     }
 
@@ -95,18 +84,4 @@ public class countryFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnListFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onListFragmentInteraction(DummyItem item);
-    }
 }

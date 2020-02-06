ba724e1328dc14a122c52ec9d1fca22934da51eb
jesus ceacero <cads918@gmail.com>
2020-02-06 21:52:58 +0100

    mapa a medias

---
+++ app/src/main/java/com/dsantano/worldquiz_app/fragments/map/MapFragment.java
@@ -0,0 +1,59 @@
+package com.dsantano.worldquiz_app.fragments.map;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dsantano.worldquiz_app.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+
+
+    public MapFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v =inflater.inflate(R.layout.fragment_map, container, false);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        return v;
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+
+        Marker m = mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(-34, 151))
+                .title("Capital")
+                .snippet("Nº habitantes, idioma")
+        );
+        m.setTag("iso-code");
+
+    }
+}
